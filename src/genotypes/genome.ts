f72fa8b452054df2d3c79ae13c8ecac4df9c8ddc
fiberwire <andrew.ramos92@gmail.com>
2017-05-07 23:08:32 -0400

    added comments

--- src/genotypes/genome.ts
+++ src/genotypes/genome.ts
@@ -6,16 +6,19 @@ import { EnomeOptions } from "./options";
 const chance = new Chance();
 export class Genome {
 
+    //the base values of the genome
     sequence: number[];
+
+    //the nucleotides derived from the base values
     nucleos: Nucleotide[];
 
     constructor(public options: EnomeOptions, public idLength: number = 12) {
-
         this.options = options;
         this.sequence = this.randomValues(options.genomeLength);
         this.nucleos = this.nucleotides;
     }
 
+    // returns a string of lower and upper case letters which are determined by the genome
     get id(): string {
         let nucleos: Nucleotide[] = this.nucleotides;
         let letters: string[];
@@ -25,13 +28,15 @@ export class Genome {
             .map((n: Nucleotide[]) =>
                 n.reduce((p: Nucleotide, n: Nucleotide) => new Nucleotide((p.value + n.value) / 2)) //average nucleotides)
             )
-            .map((n: Nucleotide) => n.interpolateLetter());
+            .map((n: Nucleotide) => n.letter());
 
 
         //return string of letters
         return letters.reduce((p, n) => p + n);
     }
 
+    //chunks values in sequence together into nucleotides, determined by options.nucleotideLength
+    //the longer the nucleotides are, the less sensitive to mutation they are.
     get nucleotides(): Nucleotide[] {
         let nucleos = _
             .chunk(this.sequence, this.options.nucleotideLength)
@@ -40,10 +45,12 @@ export class Genome {
         return nucleos;
     }
 
+    //gets the next nucleotide
     get nucleo(): Nucleotide {
         return this.nucleos.pop();
     }
 
+    //returns n random values between 0 and 1
     randomValues(n): number[] {
         const values = [];
 
--- src/genotypes/nucleotide.ts
+++ src/genotypes/nucleotide.ts
@@ -1,28 +1,69 @@
 import * as d3 from 'd3';
+import * as Chance from 'chance';
+
+const chance = new Chance();
 
 export class Nucleotide {
     constructor(public value: number) {
     }
 
 
+    //returns a float, interpolated based on this.value
     float(min: number, max: number, value: number = this.value): number {
         return d3.interpolateNumber(min, max)(value);
     }
 
+    //returns an integer, interpolated based on this.value
     int(min: number, max: number, value: number = this.value): number {
         return d3.interpolateRound(min, max)(value);
     }
 
+    //returns a natural number, interpolated based on this.value
+    natural(max: number, value: number = this.value): number {
+        return d3.interpolateRound(0, max)(value);
+    }
+
+    //returns a boolean, interpolated based on this.value
     bool(value: number = this.value): boolean {
         return this.element([true, false]);
     }
 
+    //returns an upper or lower case letter, interpolated based on this.value
     letter(value: number = this.value): string {
         let letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGIJKLMNOPQRSTUVWXYZ'.split('');
         return letters[this.int(0, letters.length - 1)];
     }
 
+    //returns a lower case letter, interpolated based on this.value
+    letterLower(value: number = this.value): string {
+        let letters = 'abcdefghijklmnopqrstuvwxyz'.split('');
+        return letters[this.int(0, letters.length - 1)];
+    }
+
+    //returns an upper case letter, interpolated based on this.value
+    letterUpper(value: number = this.value): string {
+        let letters = 'ABCDEFGIJKLMNOPQRSTUVWXYZ'.split('');
+        return letters[this.int(0, letters.length - 1)];
+    }
+
+    //returns a character, interpolated based on this.value
+    char(value: number = this.value): string {
+        let letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()'.split('');
+        return letters[this.int(0, letters.length - 1)];
+    }
+
+    //returns an element of the given array, interpolated based on this.value;
     element<T>(array: T[]): T {
         return array[this.int(0, array.length - 1)];
+    }
+
+    //returns a number of elements of the given array, the number of elements is interpolated based on this.value
+    elements<T>(array: T[]): T[] {
+        return array.slice(0, this.int(0, array.length - 1));
+    }
+
+    //returns a number of randomly selected elements of the given array, the number of elements is interpolated based on this.value
+    randomElements<T>(array: T[]): T[] {
+        return chance.shuffle(array).slice(0, this.int(0, array.length - 1));
     }
 }
