2674200641022e311f60ff50807402c123039b11
fiberwire <andrew.ramos92@gmail.com>
2017-05-10 08:39:53 -0400

    cleaned up fill operator

--- src/operators/fill.ts
+++ src/operators/fill.ts
@@ -3,14 +3,17 @@ import { EnomeOptions, Genome } from "../index";
 
 export function fill<T extends EnomeOptions>(gens: Genome<T>[], n: number): Genome<T>[] {
     while (gens.length < n) {
-        let g = new Genome({ genomeLength: 10, nucleotideLength: 1 });
+        let g = new Genome({ genomeLength: 4, nucleotideLength: 1 });
 
         //select two random parents, and create an offspring using two random weights
+
+        let p1 = g.nucleo.element(gens);
+        let p2 = g.nucleo.element(gens);
+        let w1 = g.nucleo.float(0, 1);
+        let w2 = g.nucleo.float(0, 1);
+
         gens.unshift(
-            g.nucleo.element(gens)
-                .reproduce(g.nucleo.element(gens),
-                g.nucleo.int(0, 1),
-                g.nucleo.int(0, 1))
+            p1.reproduce(p2, w1, w2),
         );
     }
 
