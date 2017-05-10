5ccfc77e061165975d1b425eecc5d2ca05b104c6
fiberwire <andrew.ramos92@gmail.com>
2017-05-10 11:37:58 -0400

    rewrote fill to be more functional

--- src/operators/fill.ts
+++ src/operators/fill.ts
@@ -1,21 +1,21 @@
 
 import { EnomeOptions, Genome } from "../index";
+import * as _ from 'lodash';
 
 export function fill<T extends EnomeOptions>(gens: Genome<T>[], n: number): Genome<T>[] {
-    while (gens.length < n) {
-        let g = new Genome({ genomeLength: 4, nucleotideLength: 1 });
+    //create offspring to fill array with
+    let offspring: Genome<T>[] =
+        _.range(0, n - gens.length)
+            .map(i => {
+                let g = new Genome({ genomeLength: 4, nucleotideLength: 1 });
 
-        //select two random parents, and create an offspring using two random weights
+                //select two random parents, and create an offspring using two random weights
+                let p1 = g.nucleo.element(gens);
+                let p2 = g.nucleo.element(gens);
+                let w1 = g.nucleo.float(0, 1);
+                let w2 = g.nucleo.float(0, 1);
+                return p1.reproduce(p2, w1, w2)
+            })
 
-        let p1 = g.nucleo.element(gens);
-        let p2 = g.nucleo.element(gens);
-        let w1 = g.nucleo.float(0, 1);
-        let w2 = g.nucleo.float(0, 1);
-
-        gens.unshift(
-            p1.reproduce(p2, w1, w2),
-        );
-    }
-
-    return gens;
+    return _.concat(gens, offspring);
 }
---
+++ src/test/operators/fill-test.ts
@@ -0,0 +1,26 @@
+
+import 'mocha';
+import { expect } from 'chai';
+import { Genome, EnomeOptions } from "../../index";
+
+import * as _ from 'lodash';
+import { fill } from "../../operators/fill";
+
+describe('operators', () => {
+    describe('fill', () => {
+
+        let gens: Genome<EnomeOptions>[];
+
+        beforeEach(() => {
+            gens = _.range(1, 5).map(i => new Genome({
+                genomeLength: 10,
+                nucleotideLength: 1
+            }));
+        })
+
+        it('should return a new array of genomes filled to a certain number with offspring of parents from original array', () => {
+            let f = fill(gens, 10);
+            expect(f.length).to.eql(10);
+        });
+    })
+})
