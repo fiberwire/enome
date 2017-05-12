f13427f5937b449bc4a429bb976f5e2c54db9712
fiberwire <andrew.ramos92@gmail.com>
2017-05-12 10:00:00 -0400

    reworked top and bottom operators to use lodash for sorting

--- src/operators/bottom.ts
+++ src/operators/bottom.ts
@@ -1,21 +1,19 @@
-import { worst } from "operators/worst";
-import { Evaluation } from "evalutation";
-import { Genome } from "genotypes/genome";
+
 import { GenomeOptions } from "options/genome-options";
+import { Genome } from "genotypes/genome";
+import { Evaluation } from "evalutation";
 import { Nucleotide } from "genotypes/nucleotide";
+
+import * as _ from 'lodash';
 
 export function bottom<T extends GenomeOptions>(
     genomes: Genome<T>[],
     threshold: number = 0.5,
     fitness: (genome: Genome<T>) => Evaluation<T>
 ): Evaluation<T>[] {
-    let w = worst(genomes, fitness);
-
-    let evals = genomes.map(fitness).sort((a, b) => {
-        if (a.fitness < b.fitness) return -1;
-        else return 1;
-    });
+    //sort evaluations of genomes by fitness, ascending order
+    let b = _.sortBy(genomes.map(fitness), e => e.fitness);
 
     //select just the ones that make the cut
-    return new Nucleotide(threshold).elements(evals);
+    return new Nucleotide(threshold).elements(b);
 }
--- src/operators/top.ts
+++ src/operators/top.ts
@@ -1,19 +1,19 @@
 
-import { Genome, GenomeOptions, Evaluation, Nucleotide } from "../index";
-import { best } from "./best";
+import { GenomeOptions } from "options/genome-options";
+import { Genome } from "genotypes/genome";
+import { Evaluation } from "evalutation";
+import { Nucleotide } from "genotypes/nucleotide";
+
+import * as _ from 'lodash';
 
 export function top<T extends GenomeOptions>(
     genomes: Genome<T>[],
     threshold: number = 0.5,
     fitness: (genome: Genome<T>) => Evaluation<T>
 ): Evaluation<T>[] {
-    let b = best(genomes, fitness);
-
-    let evals = genomes.map(fitness).sort((a, b) => {
-        if (a.fitness > b.fitness) return -1;
-        else return 1;
-    });
+    //sort evaluations of genomes by fitness, descending order
+    let t = _.sortBy(genomes.map(fitness), e => e.fitness).reverse();
 
     //select just the ones that make the cut
-    return new Nucleotide(threshold).elements(evals);
+    return new Nucleotide(threshold).elements(t);
 }
