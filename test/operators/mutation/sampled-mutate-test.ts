35fb67e84d44afb2b4b849f8e9f0db2cbe15ca03
fiberwire <andrew.ramos92@gmail.com>
2017-05-11 22:25:27 -0400

    added test for sampled-mutate

---
+++ test/operators/mutation/sampled-mutate-test.ts
@@ -0,0 +1,28 @@
+import 'mocha';
+import { expect } from 'chai';
+import { Genome } from "genotypes/genome";
+import { GenomeOptions } from "options/genome-options";
+import { Evaluation } from "evalutation";
+
+import * as _ from 'lodash';
+import { replenish } from "operators/replenish";
+import { mocks } from "../../mocks";
+import { sampledMutate } from "operators/mutation/sampled-mutate";
+
+describe('operators/mutation', () => {
+    describe('sampledMutate', () => {
+
+        let { genome, fitness } = mocks()
+
+        beforeEach(() => {
+            genome = replenish(genome);
+        });
+
+        it('should return the best of a sample of mutated genomes', () => {
+            let mutant = sampledMutate(genome, fitness);
+
+            expect(mutant.sequence.length).to.equal(genome.sequence.length);
+            expect(mutant.sequence).not.to.deep.equal(genome.sequence);
+        })
+    })
+})
