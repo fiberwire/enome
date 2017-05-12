5bbfb84cdd6c63610183881abd08e6466c5e265e
fiberwire <andrew.ramos92@gmail.com>
2017-05-11 22:44:30 -0400

    added mockGenomes and mockMutateChance to mocks

--- test/mocks.ts
+++ test/mocks.ts
@@ -8,11 +8,17 @@ import * as _ from 'lodash';
 
 export interface Mock{
     genome: Genome<GenomeOptions>,
-    fitness: (g: Genome<GenomeOptions>) => Evaluation<GenomeOptions>
+    genomes: Genome<GenomeOptions>[],
+    fitness: (g: Genome<GenomeOptions>) => Evaluation<GenomeOptions>,
+    mutateChance: number;
 }
 
 export function mockGenome(): Genome<GenomeOptions> {
     return new Genome({ genomeLength: 10, nucleotideLength: 1 });
+}
+
+export function mockGenomes(): Genome<GenomeOptions>[] {
+    return _.range(0, 10).map(i => mockGenome());
 }
 
 export function mockFitness<T extends GenomeOptions>(): (g: Genome<T>) => Evaluation<T> {
@@ -25,9 +31,15 @@ export function mockFitness<T extends GenomeOptions>(): (g: Genome<T>) => Evalua
     }
 }
 
+export function mockMutateChance(): number {
+    return 0.5;
+}
+
 export function mocks(): Mock {
     return {
         genome: mockGenome(),
-        fitness: mockFitness()
+        genomes: mockGenomes(),
+        fitness: mockFitness(),
+        mutateChance: mockMutateChance()
     }
 }
