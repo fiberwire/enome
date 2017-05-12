f6e9bfff87daa9237322ecd8d0545ef9a491d385
fiberwire <andrew.ramos92@gmail.com>
2017-05-11 23:32:47 -0400

    cleaned up reproduce test

--- test/operators/reproduction/reproduce-test.ts
+++ test/operators/reproduction/reproduce-test.ts
@@ -16,28 +16,16 @@ describe('operators/reproduction', () => {
             genome = replenish(genome);
         })
 
+        let mutated: Genome<GenomeOptions> = mutate(genome, mutateChance);
+        let offspring: Genome<GenomeOptions> = reproduce(genome, mutated);
+
         it('should produce an offspring genome with genetics from both parents', () => {
-            let mutated: Genome<GenomeOptions> = mutate(genome, mutateChance);
-
             expect(mutated.sequence.length).to.eql(genome.sequence.length);
-            expect(mutated.nucleos.length).to.eql(mutated.options.genomeLength)
-
-            let offspring: Genome<GenomeOptions> = reproduce(genome, mutated); //last two parameters are relative weights for each parents chance for their genes to be picked
-
             expect(offspring.sequence.length).to.eql(mutated.sequence.length);
-            expect(offspring.nucleos.length).to.eql(offspring.options.genomeLength);
 
-            let n: Nucleotide = offspring.nucleo;
-            expect(n.value).to.be.at.least(0);
-            expect(n.value).to.be.at.most(1);
-
-            let i: number = n.int(1, 100);
-            expect(i).to.be.at.least(1);
-            expect(i).to.be.at.most(100);
-
-            expect(genome).not.to.eql(mutated);
-            expect(genome).not.to.eql(offspring);
-            expect(mutated).not.to.eql(offspring);
+            expect(genome.sequence).to.not.deep.equal(mutated.sequence);
+            expect(genome.sequence).to.not.deep.equal(offspring.sequence);
+            expect(mutated.sequence).to.not.deep.equal(offspring.sequence);
         })
     })
 })
