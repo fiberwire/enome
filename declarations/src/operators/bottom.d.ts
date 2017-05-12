bcd8806582c80e209efa5a9706321e6259ee830d
fiberwire <andrew.ramos92@gmail.com>
2017-05-12 10:00:46 -0400

    added tests for top and worst operators

--- declarations/src/operators/bottom.d.ts
+++ declarations/src/operators/bottom.d.ts
@@ -1,4 +1,4 @@
-import { Evaluation } from "evalutation";
-import { Genome } from "genotypes/genome";
 import { GenomeOptions } from "options/genome-options";
+import { Genome } from "genotypes/genome";
+import { Evaluation } from "evalutation";
 export declare function bottom<T extends GenomeOptions>(genomes: Genome<T>[], threshold: number, fitness: (genome: Genome<T>) => Evaluation<T>): Evaluation<T>[];
--- declarations/src/operators/top.d.ts
+++ declarations/src/operators/top.d.ts
@@ -1,2 +1,4 @@
-import { Genome, GenomeOptions, Evaluation } from "../index";
+import { GenomeOptions } from "options/genome-options";
+import { Genome } from "genotypes/genome";
+import { Evaluation } from "evalutation";
 export declare function top<T extends GenomeOptions>(genomes: Genome<T>[], threshold: number, fitness: (genome: Genome<T>) => Evaluation<T>): Evaluation<T>[];
