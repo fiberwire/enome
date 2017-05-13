1de2d9bad45ba761014509d09d22d5ed0f660022
fiberwire <andrew.ramos92@gmail.com>
2017-05-13 09:13:59 -0400

    reworked safe-sampled-reproduce-many-to-one
    it now checks if offspring is better than the top 50% of genomes on average

--- src/operators/reproduction/many-to-one/safe-sampled-reproduce-many-to-one.ts
+++ src/operators/reproduction/many-to-one/safe-sampled-reproduce-many-to-one.ts
@@ -9,18 +9,20 @@ import { sampledReproduceManyToOne } from "operators/reproduction/many-to-one/sa
 
 //produce one offspring, selected from a sample, from many provided genomes, then returns the offspring if it is in the top 50%, otherwise returns the best genome
 export function safeSampledReproduceManyToOne<T extends GenomeOptions>(
-    gens: Genome<T>[],
+    genomes: Genome<T>[],
     weights: number[],
     fitness: (gen: Genome<T>) => Evaluation<T>,
     sampleSize: number = 5
 ): Genome<T> {
-    let offspring = sampledReproduceManyToOne(gens, weights, fitness, sampleSize);
-    let t = top(_.concat([offspring], gens), 0.5, fitness).map(e => e.genome);
+    let offspring = sampledReproduceManyToOne(genomes, weights, fitness, sampleSize);
+    let offspringFitness = fitness(offspring).fitness;
+    let t = top(genomes, 0.5, fitness);
+    let avgFitness = _.meanBy(t, e => e.fitness);
 
-    if (_.includes(t, offspring)) {
+    if (offspringFitness > avgFitness) {
         return offspring;
     }
     else {
-        return best(t, fitness).genome;
+        return best(genomes, fitness).genome;
     }
 }
