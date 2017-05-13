78d1e966673fc04dda3220042d9947b9c989bfe6
fiberwire <andrew.ramos92@gmail.com>
2017-05-13 09:14:46 -0400

    added tests for sample-reproduce-many-to-one and safe

--- test/operators/reproduction/many-to-one/safe-sampled-reproduce-many-to-one-test.ts
+++ test/operators/reproduction/many-to-one/safe-sampled-reproduce-many-to-one-test.ts
@@ -6,27 +6,27 @@ import { Genome } from "genotypes/genome";
 import { GenomeOptions } from "options/genome-options";
 import { replenish } from "operators/replenish";
 import { mocks } from "../../../mocks";
-import { safeReproduceManyToOne } from "operators/reproduction/many-to-one/safe-reproduce-many-to-one";
 import { top } from "operators/top";
 import { best } from "operators/best";
+import { safeSampledReproduceManyToOne } from "operators/reproduction/many-to-one/safe-sampled-reproduce-many-to-one";
 
 
 describe('operators', () => {
     describe('reproduction', () => {
-        describe('safeReproduceManyToOne', () => {
+        describe('safeSampledReproduceManyToOne', () => {
             let { genomes, fitness, weights } = mocks();
 
             beforeEach(() => {
                 genomes = genomes.map(replenish);
             })
 
-            let offspring = safeReproduceManyToOne(genomes, weights, fitness);
+            let offspring = safeSampledReproduceManyToOne(genomes, weights, fitness);
             let t = top(genomes, 0.5, fitness);
             let offspringFitness = fitness(offspring).fitness;
             let avgFitness = _.meanBy(t, e => e.fitness);
             let b = best(genomes, fitness);
 
-            it('should produce an offspring from many genomes, given a weight array', () => {
+            it('should produce an offspring from many genomes, given a weight array, selected from a sample', () => {
                 expect(offspring.sequence.length).to.eql(_.meanBy(genomes, g => g.sequence.length));
             })
 
