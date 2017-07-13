986db7cc6971ed2cb5d6ce7465a276e259cbe677
fiberwire <andrew.ramos92@gmail.com>
2017-07-13 10:59:25 -0400

    subscribed to env.state

--- src/environments/organism.ts
+++ src/environments/organism.ts
@@ -2,7 +2,9 @@ import { Environment } from "./environment";
 
 export abstract class Organism<GenType, DataType, PhenoType, EnvStateType> {
     public connectEnvironment(env: Environment<EnvStateType, GenType, DataType, PhenoType>) {
-        env.state.
+        env.state.subscribe((state) => {
+            env.state.value = this.interact(state);
+        });
     }
 
     public abstract interact(env: EnvStateType): EnvStateType;
