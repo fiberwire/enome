cba055943e97c1f39d6c1bcdf0c4251abeead885
fiberwire <andrew.ramos92@gmail.com>
2017-07-13 11:01:31 -0400

    saved connection disposable

--- src/environments/organism.ts
+++ src/environments/organism.ts
@@ -1,10 +1,15 @@
+import { IDisposable } from "rx";
 import { Environment } from "./environment";
 
 export abstract class Organism<GenType, DataType, PhenoType, EnvStateType> {
-    public connectEnvironment(env: Environment<EnvStateType, GenType, DataType, PhenoType>) {
-        env.state.subscribe((state) => {
-            env.state.value = this.interact(state);
-        });
+
+    private envConnection: IDisposable;
+
+    public connectToEnvironment(env: Environment<EnvStateType, GenType, DataType, PhenoType>) {
+        this.envConnection = env.state
+            .subscribe((state) => {
+                env.state.value = this.interact(state);
+            });
     }
 
     public abstract interact(env: EnvStateType): EnvStateType;
