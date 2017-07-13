aeb2c2998cf289ad4470988f7a9b94e43fd98469
fiberwire <andrew.ramos92@gmail.com>
2017-07-13 10:48:07 -0400

    began implementation of reactive-property

---
+++ src/reactive-property.ts
@@ -0,0 +1,26 @@
+import { BehaviorSubject, IDisposable } from 'rx';
+
+export class ReactiveProperty<T> {
+    // tslint:disable-next-line:variable-name
+    private _value: T;
+    private subject: BehaviorSubject<T>;
+
+    constructor(value: T) {
+        this.subject = new BehaviorSubject(value);
+        this._value = value;
+    }
+    
+    public get value(): T {
+        return this._value;
+    }
+
+    public set value(value: T){
+        this._value = value;
+        this.subject.onNext(value);
+    }
+
+    public subscribe(observer: (value: T) => void): IDisposable {
+        return this.subject.subscribe(observer);
+    }
+
+}
