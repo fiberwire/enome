482f1efbe0fa5959d54e7b6cfe0397a741ff97bc
fiberwire <andrew.ramos92@gmail.com>
2017-07-14 02:01:29 -0400

    reworked connection logic

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,22 +1,42 @@
-import { Subject } from "rx";
+import { IDisposable, Subject } from "rx";
+import { IEvaluation } from "../interfaces/evaluation";
+import { IGenomeOptions } from "../options/genome-options";
+import { IPopulationOptions } from "../options/population-options";
 import { ReactiveProperty } from "../reactive-property";
 import { Organism } from "./organism";
 
-export abstract class Environment<EnvStateType, T, U, V>{
+export abstract class Environment<
+    GenType extends IGenomeOptions,
+    PopType extends IPopulationOptions,
+    DataType, PhenoType, EnvStateType>{
     public state: ReactiveProperty<EnvStateType>;
-    public organisms: Array<Organism<T, U, V, EnvStateType>>;
+    public organisms: Array<Organism<GenType, PopType, DataType, PhenoType, EnvStateType>>;
+    public connections: IDisposable[];
 
-    public entry: Subject<Organism<T, U, V, EnvStateType>>;
-
-    abstract get initialState(): EnvStateType
+    public newOrganisms: Subject<Organism<GenType, PopType, DataType, PhenoType, EnvStateType>>;
+    public newConnections: Subject<IDisposable>;
+    public toEvaluate: Subject<Organism<GenType, PopType, DataType, PhenoType, EnvStateType>>;
 
     constructor() {
-        //
+        this.connectOrganisms();
     }
 
-    private connectOrganisms() {
-        this.entry.subscribe((org) => {
+    public abstract get initialState(): EnvStateType
 
+    private connectOrganisms() {
+        this.newConnections.subscribe((con) => {
+            this.connections.push(con);
         });
+
+        this.newOrganisms.subscribe((org) => {
+            org.env.value = this;
+            this.organisms.push(org);
+        });
+    }
+
+    private disconnectAll() {
+        for (const con of this.connections) {
+            con.dispose();
+        }
     }
 }
