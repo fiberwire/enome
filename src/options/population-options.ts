d035882ccaef8f3a7941a9a8d24a87fab3880877
fiberwire <andrew.ramos92@gmail.com>
2017-07-16 13:04:54 -0400

    added objective, reproduce, and mutate to population options

--- src/options/population-options.ts
+++ src/options/population-options.ts
@@ -1,5 +1,11 @@
+import { FitnessObjective } from "../../src/enums/fitness-objective";
+import { IMutateOptions } from "./mutate-options";
+import { IReproduceOptions } from "./reproduce-options";
 
 export interface IPopulationOptions {
     envs: number;
     size: number;
+    mutate: IMutateOptions;
+    reproduce: IReproduceOptions;
+    objective: FitnessObjective;
 }
