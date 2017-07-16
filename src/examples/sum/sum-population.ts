470407d2e4f9b53ab07a62a572f246570ededa13
fiberwire <andrew.ramos92@gmail.com>
2017-07-16 14:23:56 -0400

    implemented evalutate and mutate

--- src/examples/sum/sum-population.ts
+++ src/examples/sum/sum-population.ts
@@ -1,19 +1,41 @@
-import { Population } from "../../index";
+import { Genome, IEvaluation, mutate, Organism, Population } from "../../index";
 import { ISumData } from "./sum-data";
 import { ISumEnvState } from "./sum-env-state";
 import { SumEnv } from "./sum-environment";
 import { ISumGenomeOptions } from "./sum-genome-options";
+import { SumOrganism } from "./sum-organism";
 import { ISumPopOptions } from "./sum-pop-options";
 
-export class SumPopulation extends Population<
-ISumGenomeOptions, ISumPopOptions, ISumData, number[], ISumEnvState> {
+import * as _ from "lodash";
 
-    public evaluate(organism: Organism<ISumGenomeOptions, ISumPopOptions, ISumData, number[], ISumEnvState>): IEvaluation<Organism<ISumGenomeOptions, ISumPopOptions, ISumData, number[], ISumEnvState>, number[]> {
-        throw new Error("Method not implemented.");
+export class SumPopulation extends Population<
+    ISumGenomeOptions, ISumPopOptions, ISumData, number[], ISumEnvState> {
+
+    public evaluate(organism: SumOrganism): IEvaluation<SumOrganism, number[]> {
+        return {
+            fitness: Math.abs(this.popOptions.target - _.sum(organism.data.value)),
+            organism,
+        };
     }
-    public mutateOrganism(evaluation: IEvaluation<Organism<ISumGenomeOptions, ISumPopOptions, ISumData, number[], ISumEnvState>, number[]>): Genome<ISumGenomeOptions> {
-        throw new Error("Method not implemented.");
+
+    public mutate(
+        evaluation: IEvaluation<SumOrganism, number[]>,
+    ): Genome<ISumGenomeOptions> {
+        if (evaluation.fitness >= this.avgFitness.value) {
+            return mutate(
+                evaluation.organism.genotype.value,
+                this.popOptions.mutate.mutateChance * .5,
+                this.popOptions.mutate.mutateOp,
+            );
+        } else {
+            return mutate(
+                evaluation.organism.genotype.value,
+                this.popOptions.mutate.mutateChance * .5,
+                this.popOptions.mutate.mutateOp,
+            );
+        }
     }
+
     public createEnvironment(): SumEnv {
         return new SumEnv();
     }
