db586845ae47b2df19d7e72bdf78495498b14791
fiberwire <andrew.ramos92@gmail.com>
2017-07-18 01:37:29 -0400

    made ReactiveCollection not extend ReactiveProperty

--- src/reactive-collection.ts
+++ src/reactive-collection.ts
@@ -1,7 +1,8 @@
-import { BehaviorSubject, IDisposable, Observer, ReplaySubject, Subject } from "rx";
+
+import { BehaviorSubject, IDisposable, IScheduler, Observable, Observer, ReplaySubject, Subject } from "rx";
 import { ReactiveProperty } from "./index";
 
-export class ReactiveCollection<T> extends ReactiveProperty<T[]> {
+export class ReactiveCollection<T> {
 
     private pushed: ReplaySubject<T> = new ReplaySubject<T>(1);
     private popped: ReplaySubject<T> = new ReplaySubject<T>(1);
@@ -10,8 +11,18 @@ export class ReactiveCollection<T> extends ReactiveProperty<T[]> {
 
     private subs: IDisposable[];
 
-    constructor(value?: T[]) {
-        super(value);
+    private array: ReactiveProperty<T[]>;
+
+    public get value() {
+        return this.array.value;
+    }
+
+    public set value(value: T[]) {
+        this.array.value = value;
+    }
+
+    constructor(value: T[] = []) {
+        this.array = new ReactiveProperty<T[]>(value);
 
         this.subs = [
             this.subscribeToPush((pushed) => {
@@ -71,5 +82,66 @@ export class ReactiveCollection<T> extends ReactiveProperty<T[]> {
     public unshift(value: T): ReactiveCollection<T> {
         this.unshifted.onNext(value);
         return this;
+    }
+
+    public mapCollection<U>(selector: (value: T) => U): ReactiveCollection<U> {
+        return new ReactiveCollection<U>(this.value.map(selector));
+    }
+
+    public filterCollection(selector: (value: T) => boolean): ReactiveCollection<T> {
+        return new ReactiveCollection<T>(this.value.filter(selector));
+    }
+
+    public forEach(action: (value: T) => void): ReactiveCollection<T> {
+        this.value.forEach(action);
+        return this;
+    }
+
+    public subscribe(observer: (value: T[]) => void | Observer<T[]>): IDisposable {
+        return this.array.subscribe(observer);
+    }
+
+    public filter(selector: (value: T[]) => boolean) {
+        return this.array.filter(selector);
+    }
+
+    public map<U>(selector: (value: T[]) => U) {
+        return this.array.map(selector);
+    }
+
+    public debounceWithTimeout(dueTime: number, scheduler?: IScheduler) {
+        return this.array.debounce(dueTime, scheduler);
+    }
+
+    public debounceWithSelector<TTimeout>(selector: (value: T[]) => Observable<TTimeout>) {
+        return this.array.debounceWithSelector(selector);
+    }
+
+    public bufferWithTime(timeSpan: number, scheduler?: IScheduler): Observable<T[][]> {
+        return this.array.bufferWithTime(timeSpan, scheduler);
+    }
+
+    public bufferWithCount(count: number, skip?: number): Observable<T[][]> {
+        return this.array.bufferWithCount(count, skip);
+    }
+
+    public bufferWithTimeOrCount(timeSpan: number, count: number, scheduler?: IScheduler): Observable<T[][]> {
+        return this.array.bufferWithTimeOrCount(timeSpan, count, scheduler);
+    }
+
+    public asObservable(): Observable<T[]> {
+        return this.array.asObservable();
+    }
+
+    public asObserver(): Observer<T[]> {
+        return this.array.asObserver();
+    }
+
+    public asSubject(): Subject<T[]> {
+        return this.array.asSubject();
+    }
+
+    public asBehaviourSubject(): BehaviorSubject<T[]> {
+        return this.array.asBehaviourSubject();
     }
 }
