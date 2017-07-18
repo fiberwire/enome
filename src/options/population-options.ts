c16e47c84d56d3653ec94b70c7903c86a0c59271
fiberwire <andrew.ramos92@gmail.com>
2017-07-18 02:17:13 -0400

    added generations

--- src/options/population-options.ts
+++ src/options/population-options.ts
@@ -4,6 +4,7 @@ import { IReproduceOptions } from "./reproduce-options";
 
 export interface IPopulationOptions {
     size: number;
+    generations: number;
     mutate: IMutateOptions;
     objective: FitnessObjective;
     weights: {
