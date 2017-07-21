0063f58c6f756e94ec84166ecfd5bf4eeb4960a4
fiberwire <andrew.ramos92@gmail.com>
2017-07-21 10:51:44 -0400

    added start method

--- src/simulation/simulation.ts
+++ src/simulation/simulation.ts
@@ -1,9 +1,26 @@
-import { IGenomeOptions, IOrganismOptions, IPopulationOptions, Population } from "../index";
+import { Subscription } from "rxjs/Subscription";
+import {
+    Environment, IGenomeOptions, IOrganismOptions, IPopulationOptions, Population, ReactiveCollection,
+} from "../index";
 
 export class Simulation<GenType extends IGenomeOptions,
     PopType extends IPopulationOptions,
     OrgType extends IOrganismOptions,
     DataType, PhenoType, AgentStateType, EnvStateType> {
-    constructor(pops: Array<Population<GenType, PopType, OrgType, DataType, PhenoType, AgentStateType, EnvStateType>>) {
+
+    constructor(
+        public populations:
+        Array<Population<GenType, PopType, OrgType, DataType, PhenoType, AgentStateType, EnvStateType>>,
+        public environments: Array<Environment<EnvStateType>>,
+    ) {}
+
+    public start(): Subscription {
+        const subs = new Subscription();
+
+        this.populations.forEach((pop) => {
+            subs.add(pop.populate(this.environments));
+        });
+
+        return subs;
     }
 }
