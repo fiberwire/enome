3759a51d770fd751deca61132d15208095701e97
fiberwire <andrew.ramos92@gmail.com>
2017-08-08 15:09:16 -0400

    reworked to use async/await instead of rxjs

--- src/organisms/organism.ts
+++ src/organisms/organism.ts
@@ -49,126 +49,60 @@ export abstract class Organism<
         env: Observer<IStateUpdate<EState>>,
         evaluate: Observer<IEvaluation<Gen, Data, Pheno>>): Subscription {
 
-        const subs: Subscription = new Subscription();
+        return state
+            .take(this.options.interactions)
+            .map(async (s) => { // perceive, interact, and observe
+                const perception = await this.perceiveEnvironment(s);
+                const interaction = await this.interactWithPerception(perception);
+                const observation = await this.observeInteractions(interaction);
 
-        const perceptions = new ReplaySubject<IStateUpdate<AState>>();
-        const interactions = new ReplaySubject<IAgentUpdate<EState>>();
-        const evaluations = new ReplaySubject<IEvaluation<Gen, Data, Pheno>>();
-        const observations = new ReplaySubject<Data>();
+                env.next(interaction);
 
-        const perception = this.perceiveEnvironment(state.take(this.options.interactions))
-            .subscribe(
-            (p) => perceptions.next(p),
-            (error) => console.log(`perception: ${error}`),
-            () => console.log("perception completed"),
-        );
+                return observation;
+            })
+            .bufferCount(this.options.interactions) // buffer observations
+            .map((buffer) => Promise.all(buffer)) // resolve promises
+            .map(async (o) => { // evaluate
+                const evaluation = await this.evaluateObservations(o);
+                try {
+                    evaluate.next(evaluation);
+                } catch (e) {
+                    console.log(e.stack);
+                }
 
-        const interaction = this.interactWithState(perceptions.take(this.options.interactions))
-            .subscribe(
-            (i) => interactions.next(i),
-            (error) => console.log(`interaction: ${error}`),
-            () => console.log("interaction completed"),
-        );
-
-        const observation = this.observeInteractions(interactions.take(this.options.interactions))
-            .subscribe(
-            (o) => observations.next(o),
-            (error) => console.log(`observation: ${error}`),
-            () => console.log("observation completed"),
-        );
-
-        const evaluation = this.evaluateObservations(observations.take(this.options.interactions))
-            .subscribe(
-            (e) => evaluations.next(e),
-            (error) => console.log(`evaluation: ${error}`),
-            () => console.log("evaluation completed"),
-        );
-
-        const sendInteractions = interactions
-            // .take(this.options.interactions)
-            .subscribe(
-            (i) => {
-                console.log(`sending interaction to environment. #${i.interaction}: ${i.agentID}`);
-                env.next(i);
-            },
-            (error) => console.log(`sendInteraction: ${error}`),
-            () => console.log("sendInteractions completed"),
-        );
-
-        const sendEvaluations = evaluations
-            // .take(this.options.interactions)
-            .subscribe(
-            (e) => {
-                console.log(`sending evaluation to population. ${e.genotype.id}`);
-                evaluate.next(e);
-            },
-            (error) => console.log(`sendEvaluations: ${error}`),
-            () => console.log("sendEvaluations completed"),
-        );
-
-        return subs
-            .add(perception)
-            .add(interaction)
-            .add(observation)
-            .add(evaluation)
-            .add(sendInteractions)
-            .add(sendEvaluations);
+        })
+            .subscribe();
     }
 
     // turn env state into perception of env state
     // perception is just the information that is relevant to this particular organism
     // if it's a simple simulation, you would probably just set AState to be of the same
     // type as EState and return it in this.perceive()
-    private perceiveEnvironment(
-        state: Observable<IStateUpdate<EState>>,
-    ): Observable<IStateUpdate<AState>> {
-        return state
-            // .take(this.options.interactions)
-            .map((s) => {
-                return this.perceive(s);
-            })
-            .observeOn(Rx.Scheduler.asap)
-            .subscribeOn(Rx.Scheduler.asap);
+    private async perceiveEnvironment(
+        state: IStateUpdate<EState>,
+    ): Promise<IStateUpdate<AState>> {
+        return Promise
+            .resolve(state)
+            .then((s) => this.perceive(s));
     }
 
     // turn perception of env state into state that has been interacted with
-    private interactWithState(state: Observable<IStateUpdate<AState>>): Observable<IAgentUpdate<EState>> {
-        return state
-            // .take(this.options.interactions)
-            .map((s) => {
-                return this.interact(s, this.phenotype);
-            })
-            .map((i) => {
-                // add agentID to interaction
-                return { ...i, agentID: this.genotype.id };
-            })
-            .observeOn(Rx.Scheduler.asap)
-            .subscribeOn(Rx.Scheduler.asap);
+    private async interactWithPerception(state: IStateUpdate<AState>): Promise<IAgentUpdate<EState>> {
+        return Promise
+            .resolve(state)
+            .then((s) => this.interact(s, this.phenotype))
+            .then((i) => ({ ...i, agentID: this.genotype.id }));
     }
 
     // collects data from interactions with state
-    private observeInteractions(interactions: Observable<IStateUpdate<EState>>): Observable<Data> {
-        return interactions
-            .map((i) => {
-                return this.observe(i);
-            })
-            .observeOn(Rx.Scheduler.asap)
-            .subscribeOn(Rx.Scheduler.asap);
+    private async observeInteractions(interactions: IStateUpdate<EState>): Promise<Data> {
+        return Promise
+            .resolve(interactions)
+            .then((i) => this.observe(i));
     }
 
-    private evaluateObservations(
-        observations: Observable<Data>,
-    ): Observable<IEvaluation<Gen, Data, Pheno>> {
-        // const time: Observable<Data[]> = observations.bufferTime(this.options.lifeSpan);
-        const count: Observable<Data[]> = observations.bufferCount(this.options.interactions);
-        // const timeCount: Observable<Data[]> = time.race<Data[]>(count);
-
-        return count
-            .map((data) => {
-                return this.evaluate(data, this.genotype, this.phenotype);
-            })
-            .take(1)
-            .observeOn(Rx.Scheduler.asap)
-            .subscribeOn(Rx.Scheduler.asap);
+    private async evaluateObservations(observations: Promise<Data[]>): Promise<IEvaluation<Gen, Data, Pheno>> {
+        return observations
+            .then((o) => this.evaluate(o, this.genotype, this.phenotype));
     }
 }
