fda9575ca8ea606cdf2069954a53e1e40285c6d9
fiberwire <andrew.ramos92@gmail.com>
2017-08-30 13:36:31 -0400

    added artificial selection environment
    It's essentially an AgentEnvironment where the EState extends from IArtificialEState, which means it's an agent environment that has parents and specimens in the EState

---
+++ src/environments/artificial-selection.ts
@@ -0,0 +1,8 @@
+import { AgentEnvironment, IAgentUpdate, IStateUpdate } from "enviro-rx";
+import { IArtificialEState, IGenomeOptions } from "../index";
+
+export abstract class ArtificialSelection<
+    Gen extends IGenomeOptions, Pheno, AState,
+    EState extends IArtificialEState<Gen, Pheno>>
+    extends AgentEnvironment<AState, EState> {
+}
