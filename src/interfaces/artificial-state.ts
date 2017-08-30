d9a0f4be469d102826a86590d3427290e5f4de06
fiberwire <andrew.ramos92@gmail.com>
2017-08-30 13:33:02 -0400

    added artificial state interface
    contains a specimens and a parents array of IEvolvables

---
+++ src/interfaces/artificial-state.ts
@@ -0,0 +1,6 @@
+import { IEvolvable, IGenomeOptions } from "../index";
+
+export interface IArtificialEState<Gen extends IGenomeOptions, Pheno> {
+    specimens: Array<IEvolvable<Gen, Pheno>>;
+    parents: Array<IEvolvable<Gen, Pheno>>;
+}
