4798c58f1204e99cfad97a237dbb642897b181dd
fiberwire <andrew.ramos92@gmail.com>
2017-08-30 16:25:14 -0400

    added Specimen class
    Is evolvable, but can't interact with an agent environment. I plan to use them for artificial selection, since no interaction is required by them

---
+++ src/organisms/specimen.ts
@@ -0,0 +1,11 @@
+
+import { Genome, IEvolvable, IGenomeOptions } from "../index";
+
+export abstract class Specimen<Gen extends IGenomeOptions, Pheno> implements IEvolvable<Gen, Pheno> {
+    public genotype: Genome<Gen>;
+
+    public phenotype: Pheno;
+
+    public abstract createPhenotype(genotype: Genome<Gen>): Pheno;
+
+}
