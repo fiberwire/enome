09aec6146f02be715599e32dcb1b513d61f35049
fiberwire <andrew.ramos92@gmail.com>
2017-08-30 16:22:13 -0400

    made SumEnv extend AgentEnvironment

--- src/examples/sum/sum-environment.ts
+++ src/examples/sum/sum-environment.ts
@@ -6,24 +6,23 @@ import { ISumPopOptions } from "./interfaces/sum-pop-options";
 import { SumOrganism } from "./sum-organism";
 import { SumPopulation } from "./sum-population";
 
-import { Environment, Genome, IEnvironmentOptions, IStateUpdate, Organism } from "../../index";
+import { AgentEnvironment, IAgentEnvironmentOptions, IAgentUpdate, IStateUpdate } from "enviro-rx";
+import { Environment, Genome, Organism } from "../../index";
 
-export class SumEnv extends Environment<
-    ISumGenomeOptions,
-    ISumPopOptions,
-    ISumOrganismOptions,
-    ISumData, number[], ISumEnvState, ISumEnvState> {
-    public get initialState(): IStateUpdate<ISumEnvState> {
+export class SumEnv extends AgentEnvironment<ISumEnvState, ISumEnvState> {
+
+    public get defaultState(): ISumEnvState {
         return {
-            interaction: 0,
-            state: {
-                list: [],
-                sum: 0,
-            },
+            list: [],
+            sum: 0,
         };
     }
 
-    constructor(public options: IEnvironmentOptions) {
+    constructor(public options: IAgentEnvironmentOptions) {
         super(options);
+    }
+
+    public interact(interaction: IAgentUpdate<ISumEnvState>): Promise<IStateUpdate<ISumEnvState>> {
+        return Promise.resolve(interaction);
     }
 }
