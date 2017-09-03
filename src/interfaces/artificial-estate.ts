2fabf1448087894f03339696ae1c390c3bbbef08
fiberwire <andrew.ramos92@gmail.com>
2017-09-03 12:58:04 -0400

    added artificial estate
    contains specimen array and parent array

---
+++ src/interfaces/artificial-estate.ts
@@ -0,0 +1,6 @@
+import { IEvolvable, IGenomeOptions, IParentSpecimen, Specimen } from '../index';
+
+export interface IArtificialEState<Gen extends IGenomeOptions, Pheno> {
+  specimens: Array<Specimen<Gen, Pheno>>;
+  parents: Array<IParentSpecimen<Gen, Pheno>>;
+}
