78811a012f42e444a86d65f14cd5cdd61237a91a
fiberwire <andrew.ramos92@gmail.com>
2017-09-05 22:51:25 -0400

    added reversible interface
    applies to things that can turn a phenotype back into a genotype

---
+++ src/interfaces/reversible.ts
@@ -0,0 +1,8 @@
+
+import { Genome, IGenomeOptions } from "../index";
+
+export interface IReversible<Gen extends IGenomeOptions, Pheno> {
+  genotype: Genome<Gen>;
+  phenotype: Pheno;
+  createGenotype(phenotype: Pheno): Genome<Gen>;
+}
