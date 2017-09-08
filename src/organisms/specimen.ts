a3bf624facb29998fdb3d91e6db479b9bf67cddf
fiberwire <andrew.ramos92@gmail.com>
2017-09-08 10:59:36 -0400

    made ageSpecimen abstract

--- src/organisms/specimen.ts
+++ src/organisms/specimen.ts
@@ -5,19 +5,11 @@ export abstract class Specimen<Gen extends IGenomeOptions, Pheno>
   public age: number;
   public phenotype: Pheno;
 
-  constructor(public genotype: Genome<Gen>) {
+  constructor(public genotype: Genome<Gen>, age: number = 0) {
     this.phenotype = this.createPhenotype(genotype);
   }
 
   public abstract createPhenotype(genotype: Genome<Gen>): Pheno;
 
-  public ageSpecimen(n: number = 1): Specimen<Gen, Pheno> {
-    return {
-      age: this.age + n,
-      ageSpecimen: this.ageSpecimen,
-      createPhenotype: this.createPhenotype,
-      genotype: this.genotype,
-      phenotype: this.phenotype,
-    };
-  }
+  public abstract ageSpecimen(n: number): Specimen<Gen, Pheno>;
 }
