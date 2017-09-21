14c85964b3adf80b842b09d1e7625f1a60dbcd9f
fiberwire <andrew.ramos92@gmail.com>
2017-09-21 17:59:17 -0400

    cleaned up organism interface

--- src/interfaces/organism.ts
+++ src/interfaces/organism.ts
@@ -1,14 +1,10 @@
 import { IAgent } from 'enviro-rx';
-import {
-  IGenomeOptions,
-  INaturalAState,
-  INaturalEState,
-  ISpecimen,
-} from '../index';
+import { IGenomeOptions, ISpecimen } from '../index';
 
 // tslint:disable-next-line:no-empty-interface
 export interface IOrganism<
   Gen extends IGenomeOptions,
   Pheno extends IAgent<AState, EState>,
-  AState, EState>
-  extends ISpecimen<Gen, Pheno> { }
+  AState,
+  EState
+> extends ISpecimen<Gen, Pheno> {}
