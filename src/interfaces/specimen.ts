346273699b1403fd6be8b36d287f438efa6bb5da
fiberwire <andrew.ramos92@gmail.com>
2017-09-21 17:59:27 -0400

    added fitness to specimen interface

--- src/interfaces/specimen.ts
+++ src/interfaces/specimen.ts
@@ -3,6 +3,7 @@ import { Genome, IEvolvable, IGenomeOptions } from '../index';
 export interface ISpecimen<Gen extends IGenomeOptions, Pheno>
   extends IEvolvable<Gen, Pheno> {
   age: number;
+  fitness?: number;
 
   createPhenotype(genotype: Genome<Gen>): Pheno;
   ageSpecimen(n: number): ISpecimen<Gen, Pheno>;
