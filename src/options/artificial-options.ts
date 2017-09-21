3ee0f51b424dd58e0a0ca9c7ba5269f7fc470da1
fiberwire <andrew.ramos92@gmail.com>
2017-09-21 18:00:59 -0400

    made artificial options extend population and agent env options

--- src/options/artificial-options.ts
+++ src/options/artificial-options.ts
@@ -1,6 +1,5 @@
 import { IAgentEnvironmentOptions } from 'enviro-rx';
+import { IPopulationOptions } from '../index';
 
-export interface IArtificialOptions extends IAgentEnvironmentOptions {
-  parents: number;
-  specimens: number;
-}
+// tslint:disable-next-line:no-empty-interface
+export interface IArtificialOptions extends IPopulationOptions, IAgentEnvironmentOptions {}
