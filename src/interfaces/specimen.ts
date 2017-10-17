7b68dce1217cbe228474e254f420e178a06cd2ea
fiberwire <andrew.ramos92@gmail.com>
2017-10-17 14:25:17 -0400

    added genotype to specimen interface

--- src/interfaces/specimen.ts
+++ src/interfaces/specimen.ts
@@ -3,7 +3,8 @@ import { Genome, IEvolvable, IGenomeOptions } from '../index';
 export interface ISpecimen<Gen extends IGenomeOptions, Pheno>
   extends IEvolvable<Gen, Pheno> {
   age: number;
-
+  genotype: Genome<Gen>;
+  
   createPhenotype(genotype: Genome<Gen>): Pheno;
   ageSpecimen(n: number): ISpecimen<Gen, Pheno>;
 }
