44c0eeb2f611aa5fca801963c04ed26636a3403c
fiberwire <andrew.ramos92@gmail.com>
2017-10-18 00:13:15 -0400

    cleaned up cloneEvaluation

--- src/operators/cloneEvaluation.ts
+++ src/operators/cloneEvaluation.ts
@@ -1,15 +1,8 @@
 import * as _ from 'lodash';
 
-import {
-  IEvaluation,
-  IGenomeOptions,
-} from '../index';
+import { IEvaluation, IGenomeOptions } from '../index';
 
-export function cloneEvaluation<
-  Gen extends IGenomeOptions,
-  Data,
-  Pheno
->(
+export function cloneEvaluation<Gen extends IGenomeOptions, Data, Pheno>(
   evaluation: IEvaluation<Gen, Data, Pheno>
 ): IEvaluation<Gen, Data, Pheno> {
   return _.cloneDeep(evaluation);
